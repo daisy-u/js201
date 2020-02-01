b183718c0ab4efe9fc7241c0b28996be4af45e34
Daisy Umotong <x4daisy@rocketmail.com>
2020-02-01 14:15:31 -0600

    exercise 201

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -3,9 +3,17 @@
 // Example: hello('Mustache') should return 'Hello, Mustache!'
 // Hey Flex Class, it'd s Patrick
 // Great question
-
+function hello (name) {
+    return 'Hello, ' + name + '!';
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+function helloDefault (name) {
+   if (name) {
+       return 'Hello, ' + name + '!'
+     }
+  else return 'Hello, world!';
+}
